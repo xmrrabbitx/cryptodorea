eaa798cf79f2afecef467197b036e849f2ae06c0
xmrrabbitx <hadi.mirzaie300@gmail.com>
2024-01-06 22:38:19 +0330

    add icon

--- admin/admin.php
+++ admin/admin.php
@@ -9,23 +9,37 @@ add_action('admin_menu', 'dorea_add_menu_page');
 
 function dorea_add_menu_page() {
 
-    add_menu_page(
-        'Dorea Cash Back',   // Page title
-        'Dorea Cash Back',        // Menu title
-        'manage_options',     // Capability required to access
-        'your-plugin-slug',   // Menu slug (unique identifier)
-        'dorea_main_page_content', // Callback function to display page content
-        'dashicons-admin-plugins',  // Icon URL or dashicon class
-        20 // Menu position (you can adjust this)
-    );
+   
 
+    $svg_path = plugin_dir_path(__FILE__) . 'icons/doreaLogo.svg';
+
+    if (file_exists($svg_path)) {
+        $svg_content = file_get_contents($svg_path);
+        $base64_encoded = base64_encode($svg_content);
+
+        add_menu_page(
+            'Dorea Cash Back',   // Page title
+            'Dorea Cash Back',        // Menu title
+            'manage_options',     // Capability required to access
+            'crypto-dorea-cashback',   // Menu slug (unique identifier)
+            'dorea_main_page_content', // Callback function to display page content
+            'data:image/svg+xml;base64,' . $base64_encoded, // Icon URL or dashicon class
+            20 // Menu position (you can adjust this)
+        );
+    
+    }
+    
 }
 
 function dorea_main_page_content(){
 
     print("create cash back program");
     print("</br>");
-    print("set public key");
-    print("</br>");
-    print("set private key ");
+    print("
+        <form type='POST' action=''>
+            <label>expire</label>
+            <input type='text'>
+            <button type='submit'>set expire date</button>
+        </form>
+    ");
 }
