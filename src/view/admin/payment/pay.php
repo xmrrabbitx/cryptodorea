195d9f8af7dba0e2f598d2066740181f9dbb492d
xmrrabbitx <hadi.mirzaie300@gmail.com>
2024-09-09 01:07:27 +0330

    fix eligibility issues

--- src/view/admin/payment/pay.php
+++ src/view/admin/payment/pay.php
@@ -410,7 +410,7 @@ function dorea_admin_pay_campaign()
 
                             print ("<span class='!pl-3 !pt-1 !col-span-1 !mx-auto'>");
 
-                            if($userEther <= (float)$contractAmount){
+                            //if($userEther <= (float)$contractAmount){
 
                                     $sumUserEthers[] = $userEther;
                                     if(array_sum($sumUserEthers) <= $contractAmount){
@@ -420,6 +420,7 @@ function dorea_admin_pay_campaign()
                                         $qualifiedWalletAddresses[] = $campaignUser[$cashbackName]['walletAddress'];
                                         $usersList[] = $users;
 
+
                                         print("
                                             <svg class='size-5 text-green-500' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor'>
                                               <path fill-rule='evenodd' d='M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z' clip-rule='evenodd' />
@@ -427,36 +428,41 @@ function dorea_admin_pay_campaign()
                                         ");
 
                                     }else{
+
                                         print("
                                             <svg class='size-5 text-amber-500' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor'>
                                               <path fill-rule='evenodd' d='M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z' clip-rule='evenodd' />
                                             </svg>
                                         ");
+
                                     }
 
-                                }elseif((int)$contractAmount===0){
+                           // }elseif((int)$contractAmount===0){
+                                /*
                                     print("
                                        <svg class='size-5 text-green-500' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor'>
                                          <path fill-rule='evenodd' d='M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z' clip-rule='evenodd' />
                                        </svg>
                                     ");
-                                }else{
+                                */
+                           // }else{
+                                /*
                                     print("
                                         <svg class='size-5 text-amber-500' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor'>
                                           <path fill-rule='evenodd' d='M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z' clip-rule='evenodd' />
                                         </svg>
                                     ");
-                                }
-                                print ("
+                                */
+                           // }
+                            print ("
                                     </span>
                                   </div>
-                                ");
+                            ");
 
                     }
 
 
             }
-
         }
 
         print("</div>");
