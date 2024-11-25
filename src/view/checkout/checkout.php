77bfb69913f8ab1136577820c22a14e9cfd06151
xmrrabbitx <hadi.mirzaie300@gmail.com>
2024-11-26 02:09:53 +0330

    add legacy code

--- src/view/checkout/checkout.php
+++ src/view/checkout/checkout.php
@@ -13,45 +13,103 @@ function cashback(): void
     static $contractAddressConfirm;
 
     if(is_checkout()) {
-        if (has_filter('woocommerce_checkout_fields')) {
+        if (!WC()->cart->get_cart_contents_count() == 0) {
 
-            add_filter('woocommerce_checkout_fields', 'customize_checkout', 10);
-            function customize_checkout()
-            {
+            // load claim campaign style
+            wp_enqueue_style('DOREA_CHECKOUT_STYLE', plugins_url('/cryptodorea/css/checkout.css'));
 
-            }
+            // get cashback list of admin
+            $cashback = new cashbackController();
+            $cashbackList = $cashback->list();
 
-            // legacy code here ...
+            // get campaign list of user
+            $checkoutController = new checkoutController;
+            $diffCampaignsList = $checkoutController->check($cashbackList);
 
-        } else {
 
-            if (!WC()->cart->get_cart_contents_count() == 0) {
+            // check on Authentication user
+            if (is_user_logged_in()) {
 
-                // load claim campaign style
-                wp_enqueue_style('DOREA_CHECKOUT_STYLE', plugins_url('/cryptodorea/css/checkout.css'));
+                if ($cashbackList) {
 
-                // get cashback list of admin
-                $cashback = new cashbackController();
-                $cashbackList = $cashback->list();
+                    if (has_filter('woocommerce_checkout_fields')) {
 
-                // get campaign list of user
-                $checkoutController = new checkoutController;
+                        add_filter( 'woocommerce_form_field', 'bbloomer_remove_optional_checkout_fields', 9999 );
+                        function bbloomer_remove_optional_checkout_fields( $fields) {
+                            $fields = preg_replace('//', '', $fields);
+                            return $fields;
+                        }
 
-                $diffCampaignsList = $checkoutController->check($cashbackList);
+                        add_filter('woocommerce_after_order_notes', 'customize_checkout', 10,2);
+                        function customize_checkout($checkout)
+                        {
+                            // get cashback list of admin
+                            $cashback = new cashbackController();
+                            $cashbackList = $cashback->list();
 
-                // check on Authentication user
-                if (is_user_logged_in()) {
+                            // get campaign list of user
+                            $checkoutController = new checkoutController;
+                            $diffCampaignsList = $checkoutController->check($cashbackList);
 
-                    if ($cashbackList) {
+                            if (!empty($diffCampaignsList)) {
+
+                                // show campaigns in checkout page
+                                if (!empty($cashbackList)) {
+
+                                    print('<div id="dorea_campaigns_checkout"><h3>' . esc_html__('Join to Cashback Campaign') . '</h3>');
+
+                                    foreach ($diffCampaignsList as $campaign) {
+
+                                        $campaignInfo = get_transient($campaign);
+
+                                        // check if any campaign funded or not!
+                                        if (get_option($campaign . '_contract_address')) {
+                                            // check if campaign started or not
+                                            if ($checkoutController->expire($campaign)) {
+                                                woocommerce_form_field(
+                                                    'dorea_campaigns_'.$campaignInfo['campaignNameLable'],
+                                                    array(
+                                                        'type' => 'checkbox',
+                                                        'class' => array('dorea-campaigns-class form-row-wide'),
+                                                        'label' => __($campaignInfo['campaignNameLable']),
+                                                        'required' => false,
+                                                        'custom_attributes' => array('optional' => false)
+                                                    ),
+                                                    $checkout->get_value('dorea_campaigns')
+                                                );
+                                            }
+                                        }
+                                    }
+                                    woocommerce_form_field(
+                                        'dorea_wallet_address',
+                                        array(
+                                            'type' => 'text',
+                                            'class' => array('dorea-wallet-address-class form-row-wide'),
+                                            'label' => __('wallet address'),
+                                            'placeholder' => __('Enter Wallet Address...'),
+                                            'required' => false,
+                                        ),
+                                        $checkout->get_value('dorea_wallet_address')
+                                    );
+
+                                    print('</div>');
+                                }
+                            }
+
+                        }
+
+                        // legacy code here ...
+
+                    } else {
 
                         print("
-                            <div class='!fixed !mx-auto !left-0 !right-0 !top-[20%] !bg-white !w-96 shadow-[0_5px_25px_-15px_rgba(0,0,0,0.3)] !p-7 !rounded-md !text-center !border' id='doreaCheckout' style='padding-left:10px;'>
-                                <span id='doreaClose'>
-                                   <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='size-6 !text-rose-400 !cursor-pointer !hover:text-rose-200 !float-right'>
-                                       <path stroke-linecap='round' stroke-linejoin='round' d='M6 18 18 6M6 6l12 12' />
-                                   </svg>
-                               </span>
-                        ");
+                                        <div class='!fixed !mx-auto !left-0 !right-0 !top-[20%] !bg-white !w-96 shadow-[0_5px_25px_-15px_rgba(0,0,0,0.3)] !p-7 !rounded-md !text-center !border' id='doreaCheckout' style='padding-left:10px;'>
+                                            <span id='doreaClose'>
+                                               <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='size-6 !text-rose-400 !cursor-pointer !hover:text-rose-200 !float-right'>
+                                                   <path stroke-linecap='round' stroke-linejoin='round' d='M6 18 18 6M6 6l12 12' />
+                                               </svg>
+                                           </span>
+                                    ");
                         if (!empty($diffCampaignsList)) {
 
                             $addtoCashback = true;
@@ -73,37 +131,37 @@ function cashback(): void
                                             // add to cash back program option
                                             if ($addtoCashback) {
                                                 print("
-                                                   <h3 class='!text-lg'>Join to Cashback Campaign</h3> 
-                                                   <div class='!grid !grid-cols-1 !gap-2'>
-                                                        <label class='!text-sm'>choose which campaign you want to participate in:</label>
-                                                        <div id='doreaCampaignsSection' class='!grid !grid-cols-1 !pb-5 !p-3  !w-auto !ml-1 !mr-1 !p-2 !col-span-1 !mt-2 !rounded-sm !border border-slate-700 !float-left'>
-                                                ");
+                                                               <h3 class='!text-lg'>Join to Cashback Campaign</h3> 
+                                                               <div class='!grid !grid-cols-1 !gap-2'>
+                                                                    <label class='!text-sm'>choose which campaign you want to participate in:</label>
+                                                                    <div id='doreaCampaignsSection' class='!grid !grid-cols-1 !pb-5 !p-3  !w-auto !ml-1 !mr-1 !p-2 !col-span-1 !mt-2 !rounded-sm !border border-slate-700 !float-left'>
+                                                            ");
                                                 $addtoCashback = false;
                                             }
 
                                             print(" 
-                                                <div class='!flex !mt-1'>
-                                                    <div class='!w-1/12 !ml-1'><input id='dorea_add_to_cashback_checkbox' class='dorea_add_to_cashback_checkbox_ !accent-white !text-white !mt-1 !cursor-pointer' type='checkbox' value='" . esc_js($campaign) . "'></div>
-                                                    <label class='!w-11/12 !pl-3 !text-left !ml-0 xl:!text-sm lg:!text-sm md:!text-sm sm:!text-sm !text-[12px] !float-left !content-center !whitespace-break-spaces'>" . $campaignInfo['campaignNameLable'] . "</label>
-                                                </div>
-                                            ");
+                                                            <div class='!flex !mt-1'>
+                                                                <div class='!w-1/12 !ml-1'><input id='dorea_add_to_cashback_checkbox' class='dorea_add_to_cashback_checkbox_ !accent-white !text-white !mt-1 !cursor-pointer' type='checkbox' value='" . esc_js($campaign) . "'></div>
+                                                                <label class='!w-11/12 !pl-3 !text-left !ml-0 xl:!text-sm lg:!text-sm md:!text-sm sm:!text-sm !text-[12px] !float-left !content-center !whitespace-break-spaces'>" . $campaignInfo['campaignNameLable'] . "</label>
+                                                            </div>
+                                                        ");
 
                                         }
                                     }
                                 }
                             }
-                        }else{
+                        } else {
                             print('<p id="doreaNoCampaign"></p>');
                         }
 
                         if ($contractAddressConfirm) {
                             print('</div> 
-                                    <div class="!col-span-1 !mt-2">
-                                        <p class="!text-sm !mt-3" id="dorea_error" style="display:none;color:#ff5d5d;"></p>
-                                       <input class="!p-3 !text-sm !mt-1 !ml-1 !bg-white !shadow-none !rounded-md" id="dorea_walletaddress" type="text" placeholder="wallet address...">
-                                       <button id="doreaChkConfirm" class="!rounded !mt-3 !pl-5 !pr-5 !pt-3 !pb-3">Join</button>
-                                    </div> 
-                                 </div>');
+                                                <div class="!col-span-1 !mt-2">
+                                                    <p class="!text-sm !mt-3" id="dorea_error" style="display:none;color:#ff5d5d;"></p>
+                                                   <input class="!p-3 !text-sm !mt-1 !ml-1 !bg-white !shadow-none !rounded-md" id="dorea_walletaddress" type="text" placeholder="wallet address...">
+                                                   <button id="doreaChkConfirm" class="!rounded !mt-3 !pl-5 !pr-5 !pt-3 !pb-3">Join</button>
+                                                </div> 
+                                             </div>');
 
                         }
 
@@ -118,7 +176,6 @@ function cashback(): void
             }
         }
     }
-
 }
 
 
