2478c00bfc1e66320f5fef5a2c06e166fc776958
xmrrabbitx <hadi.mirzaie300@gmail.com>
2025-01-24 19:20:52 +0330

    replace guzzle with wp_remote_get

--- src/utilities/ethHelper.php
+++ src/utilities/ethHelper.php
@@ -27,18 +27,20 @@ class ethHelper
 
         for ($i = 0; $i <= 3; $i++) {
             try {
-                $res = $client->request('GET', 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR');
-                $res->getStatusCode();
-                $res->getHeaderLine('content-type');
-                $body = $res->getBody();
-                $json = json_decode($body);
-
-                if (!empty($json)) {
-                    break;
+                $response = wp_remote_get('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR');
+                if(!empty($response)) {
+                    if(!isset($response->errors)) {
+                        if (isset($response['body'])) {
+                            $json = json_decode($response['body']);
+                            if (!empty($json)) {
+                                break;
+                            }
+                        }
+                    }
                 }
             }catch (Exception $error) {
                 if ($i == 3) {
-                    if (empty($json)){
+                    if (empty($response)){
                         // throw error on Guzzle response
                         error_log($error);
                         print("<span>Something went wrong! please refresh the page...</span>");
@@ -49,12 +51,10 @@ class ethHelper
         }
 
         // throw error on api response
-        if(isset($json->Response)) {
-            if ($json->Response === "Error") {
-                error_log($json->Message);
-                print("<span>Something went wrong! please refresh the page...</span>");
-                exit;
-            }
+        if(isset($response->errors)) {
+            error_log($response->errors['http_request_failed'][0]);
+            print("<span>Something went wrong! please refresh the page...</span>");
+            exit;
         }
 
         return $json->USD;
--- src/view/admin/payment/pay.php
+++ src/view/admin/payment/pay.php
@@ -178,10 +178,9 @@ function dorea_admin_pay_campaign():void
 
                 $campaignUser = get_option('dorea_campaigninfo_user_' . $users);
 
-                //$ethBasePrice = bcdiv(1 , ethHelper::ethPrice(),10);
-
+                $ethBasePrice = bcdiv(1 , ethHelper::ethPrice(),10);
                 //hypothetical price of eth _ get this from an online service
-                $ethBasePrice = 0.0004;
+                //$ethBasePrice = 0.0004;
 
                 if($ethBasePrice) {
                     if ($campaignUser && $campaignUser[$cashbackName]['purchaseCounts'] >= $shoppingCount) {
