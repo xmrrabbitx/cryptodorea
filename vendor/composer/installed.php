d488a5304aa12cdcd3a3bde2cb487669e21a0e8e
xmrrabbitx <hadi.mirzaie300@gmail.com>
2025-01-26 13:53:24 +0330

    fix issues

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'cryptodorea/doreacashback',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '80a955af96b82b2b528787278fd14ab8420c0d50',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cryptodorea/doreacashback' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '80a955af96b82b2b528787278fd14ab8420c0d50',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
