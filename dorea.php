177aba4646fdfe9897390a420d358bed7f69402e
xmrrabbitx <hadi.mirzaie300@gmail.com>
2025-01-30 15:03:52 +0330

    fix issues

--- dorea.php
+++ dorea.php
@@ -4,7 +4,7 @@
  * License: GNU General Public License v2 or later
  *
  * Plugin Name: Crypto Dorea: Crypto Cashback for WooCommerce
- * Description: A New Innovative Crypto Cash Back for the most loyal customers
+ * Description: A New Innovative Crypto CashBack for the most loyal customers
  * Version: 1.0.0
  * Author: Crypto Dorea Team
  * Author URI: https://cryptodorea.io
